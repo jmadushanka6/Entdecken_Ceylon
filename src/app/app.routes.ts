4977646f0d66efb4824b199cd03bf4b2e1a78bad
jmadushanka6 <janitha.pm6@gmail.com>
2026-02-23 11:43:04 +0100

    Add admin standalone page form with content service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page.component';
 import { PrivacyPolicyComponent } from './privacy-policy.component';
+import { AdminPageComponent } from './admin-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'datenschutz', component: PrivacyPolicyComponent },
+  { path: 'admin', component: AdminPageComponent },
   { path: '**', redirectTo: '' }
 ];
