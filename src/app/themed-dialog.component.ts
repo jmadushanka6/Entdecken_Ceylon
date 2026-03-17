658b41d0ba4410cab3e9c0f1a456ee48dd28242d
jmadushanka6 <janitha.pm6@gmail.com>
2026-03-17 21:57:05 +0100

    Refine themed dialog to use native dialog and project palette

--- src/app/themed-dialog.component.ts
+++ src/app/themed-dialog.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, effect } from '@angular/core';
 import { ThemedDialogService } from './themed-dialog.service';
 
 @Component({
@@ -9,6 +9,52 @@ import { ThemedDialogService } from './themed-dialog.service';
   templateUrl: './themed-dialog.component.html',
   styleUrl: './themed-dialog.component.scss'
 })
-export class ThemedDialogComponent {
-  constructor(readonly themedDialogService: ThemedDialogService) {}
+export class ThemedDialogComponent implements AfterViewInit {
+  @ViewChild('dialogElement')
+  private dialogElement?: ElementRef<HTMLDialogElement>;
+
+  constructor(readonly themedDialogService: ThemedDialogService) {
+    effect(() => {
+      const activeDialog = this.themedDialogService.activeDialog();
+      this.syncDialogVisibility(activeDialog !== null);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.syncDialogVisibility(this.themedDialogService.activeDialog() !== null);
+  }
+
+  onDialogCancel(event: Event): void {
+    event.preventDefault();
+    this.themedDialogService.dismiss();
+  }
+
+  onDialogClick(event: MouseEvent): void {
+    const dialog = this.dialogElement?.nativeElement;
+
+    if (!dialog) {
+      return;
+    }
+
+    if (event.target === dialog) {
+      this.themedDialogService.dismiss();
+    }
+  }
+
+  private syncDialogVisibility(shouldBeOpen: boolean): void {
+    const dialog = this.dialogElement?.nativeElement;
+
+    if (!dialog) {
+      return;
+    }
+
+    if (shouldBeOpen && !dialog.open) {
+      dialog.showModal();
+      return;
+    }
+
+    if (!shouldBeOpen && dialog.open) {
+      dialog.close();
+    }
+  }
 }
